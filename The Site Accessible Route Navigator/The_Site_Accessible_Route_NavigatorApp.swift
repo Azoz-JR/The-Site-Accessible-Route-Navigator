a29c15df8464e4511235ac3e692a23f1a83899ef
Abdelaziz Salah <77539943+Azoz-JR@users.noreply.github.com>
2025-11-25 16:53:43 +0100

    Initial Commit

---
+++ The Site Accessible Route Navigator/The_Site_Accessible_Route_NavigatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  The_Site_Accessible_Route_NavigatorApp.swift
+//  The Site Accessible Route Navigator
+//
+//  Created by Abdelaziz Salah on 25/11/25.
+//
+
+import SwiftUI
+
+@main
+struct The_Site_Accessible_Route_NavigatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
